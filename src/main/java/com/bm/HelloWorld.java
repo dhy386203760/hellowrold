431ee02a8dbc845a17f4beead9f3135bcd0effcf
董皓月 <donghaoyue@bmsoft.com.cn>
2019-10-21 15:57:18 +0800

    git init

---
+++ src/main/java/com/bm/HelloWorld.java
@@ -0,0 +1,18 @@
+package com.bm;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * @author dhy
+ * @version 1.0
+ * @ClassName: HelloWorld
+ * @createTime: 2019/10/21 15:43
+ * @since JDK 1.8
+ */
+@SpringBootApplication
+public class HelloWorld {
+    public static void main(String[] args) {
+        SpringApplication.run(HelloWorld.class, args);
+    }
+}
---
+++ src/main/java/com/bm/controller/HelloController.java
@@ -0,0 +1,20 @@
+package com.bm.controller;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+/**
+ * @author dhy
+ * @version 1.0
+ * @ClassName: HelloController
+ * @createTime: 2019/10/21 15:44
+ * @since JDK 1.8
+ */
+@RestController
+public class HelloController {
+
+    @GetMapping("/hello")
+    public String hello(String name){
+        return "hello:"+name;
+    }
+}
